6f21efb722dbcc9e818d564bc2e4b494640241b3
Parker <parker.seaman@trojans.dsu.edu>
2017-04-18 10:20:37 -0500

    now it actually works
    I'm bad at github

--- cpu_gov.sh
+++ cpu_gov.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 core=0
 while [ $core != 3 ]; do
-powersave > /sys/devices/system/cpu/cpu$core/cpufreq/scaling_governor
+echo powersave > /sys/devices/system/cpu/cpu$core/cpufreq/scaling_governor
 let core=core+1
-
 done
