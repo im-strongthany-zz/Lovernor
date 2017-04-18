cb8833a50f3564cc7db9a5bbbe8f07c98cc36e37
Parker <parker.seaman@trojans.dsu.edu>
2017-04-18 10:30:55 -0500

    more features!
    - provides feedback
    - right number of cores (for a 2 core hyperthreaded 2nd gen i5)
    - colors!!

--- cpu_gov.sh
+++ cpu_gov.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 core=0
-while [ $core != 3 ]; do
-echo powersave > /sys/devices/system/cpu/cpu$core/cpufreq/scaling_governor
+governor=powersave
+while [ $core != 4 ]; do
+echo $governor > /sys/devices/system/cpu/cpu$core/cpufreq/scaling_governor
+echo -e "\e[1;35m Processor core number $core is now $governor \e[0m"
 let core=core+1
 done
+
+#https://unix.stackexchange.com/questions/43408/printing-colored-text-using-echo
