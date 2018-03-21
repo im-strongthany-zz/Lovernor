8a409474b89eaad967660f78fc210d2ba14c2387
Parker <parker.seaman@trojans.dsu.edu>
2018-03-21 13:18:40 -0500

    Updated for more features
    Added choice between powersave and performance

--- cpu_gov.sh
+++ cpu_gov.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 core=0
-governor=powersave
+read -p 'Hello, would you like power[s]ave, or [p]erformance?: ' input
+if [ $input == "s" ]; then
+	governor="powersave"
+elif [ $input == "p" ]; then
+	governor="performance"
+fi
 while [ $core != 4 ]; do
-echo $governor > /sys/devices/system/cpu/cpu$core/cpufreq/scaling_governor
+sudo echo $governor > /sys/devices/system/cpu/cpu$core/cpufreq/scaling_governor
 echo -e "\e[1;35m Processor core number $core is now $governor \e[0m"
 let core=core+1
 done
 
-#https://unix.stackexchange.com/questions/43408/printing-colored-text-using-echo
